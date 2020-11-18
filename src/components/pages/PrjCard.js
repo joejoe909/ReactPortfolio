fe8ae738188343d64dd8bd67637bc47692abe8a4
Joe Farrish <joseph_a_f@hotmail.com>
2020-11-17 21:03:11 -0700

    updated my repos, added git hub links

--- src/components/pages/PrjCard.js
+++ src/components/pages/PrjCard.js
@@ -7,6 +7,7 @@ function PrjCard(props) {
         
         <div className="col-md-4">
             <a href={props.PrjLink} target="_blank" rel="noopener noreferrer" > <img src={props.ImgLink} className="img-fluid p-4 m-2" alt=""/> </a>
+            <a href={props.GitHub} target="_blank" rel="">GitHub Repo</a>
             <h6>{props.prj}</h6>
             <p>{props.desc}</p>
         </div>
